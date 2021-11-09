bcfa8f1e9cab600d8e8750473879bd4e44495075
LeaPhant <mail@lea.moe>
2021-11-09 20:30:29 +0100

    Add compatibility for CommonJS

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+export default {
+    input: 'src/index.js',
+    output: [{
+        file: './dist/index.cjs',
+        format: 'cjs'
+    }, {
+        file: './dist/index.mjs',
+        format: 'es'
+    }],
+    external: ['node-fetch'] 
+  };
+  
